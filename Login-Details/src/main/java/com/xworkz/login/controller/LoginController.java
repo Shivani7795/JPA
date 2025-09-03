f7ebda62a12bb3aacf3c3319a3be757323919948
Shivani <shivani332433@gmail.com>
2025-09-03 23:49:12 +0530

    Login details

--- Login-Details/src/main/java/com/xworkz/login/controller/LoginController.java
+++ Login-Details/src/main/java/com/xworkz/login/controller/LoginController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
 
@@ -20,12 +21,13 @@ public class LoginController {
     @Autowired
     private LoginService loginService;
 
-    @GetMapping
+    @GetMapping("getIndex")
     public String register(){
         System.out.println("register method in controller");
         return "register";
     }
 
+    @GetMapping("redirecTologin")
     public String Login(){
         System.out.println("Login method in controller");
         return "login";
@@ -42,6 +44,28 @@ public class LoginController {
             model.addAttribute("dto",dto);
             return "register";
         }
-        if(loginService.s)
+        if(loginService.save(dto)){
+            model.addAttribute("message","Invalid details");
+            model.addAttribute("dto",dto);
+            return "register";
+        }
+        if(loginService.save(dto)){
+            model.addAttribute("message","submitted");
+        }else{
+            model.addAttribute("message","not submitted");
+            model.addAttribute("dto",dto);
+        }
+        return "register";
+    }
+    public String login(@RequestParam String email,@RequestParam String password,Model model){
+        System.out.println("controller login");
+        System.out.println("email:"+email+"Password:"+password);
+        LoginDto dto = loginService.acceptLogin(email, password);
+        if (dto == null){
+            model.addAttribute("message","invalid email or password");
+            return "login";
+        }
+        model.addAttribute("dto",dto);
+        return "view";
     }
 }
