999b39709b50770bf2edac3591d073d8c28b07fa
Shivani <shivani332433@gmail.com>
2025-09-08 23:34:40 +0530

    saving data

--- tourism-tourism/src/main/java/com/xworkz/tourism/config/TourismConfiguration.java
+++ tourism-tourism/src/main/java/com/xworkz/tourism/config/TourismConfiguration.java
@@ -1,17 +1,31 @@
 package com.xworkz.tourism.config;
 
+import org.hibernate.cfg.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import javax.activation.DataSource;
+import java.util.Properties;
+
 @Configuration
 @ComponentScan("com.xworkz.tourism")
 @EnableWebMvc
+@PropertySource("classpath:application.properties")
 public class TourismConfiguration implements WebMvcConfigurer {
+
+    @Autowired
+    private Environment environment;
+    public TourismConfiguration(){
+        System.out.println("Tourism const");
+    }
+
     @Bean
     public InternalResourceViewResolver internalResourceViewResolver()
     {
@@ -26,6 +40,38 @@ public class TourismConfiguration implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/image/**").addResourceLocations("/image/");
         registry.addResourceHandler("/js/**").addResourceLocations("/js/");
-
     }
-}
+    
+        @Bean
+        public LocalContainerEntityManagerFactoryBean containerEntityManagerFactoryBean()
+        {
+            LocalContainerEntityManagerFactoryBean entityManagerFactoryBean=new LocalContainerEntityManagerFactoryBean();
+            entityManagerFactoryBean.setDataSource(dataSource());
+            HibernateJpaVendorAdapter hibernateJpaVendorAdapter=new HibernateJpaVendorAdapter();
+            entityManagerFactoryBean.setJpaVendorAdapter(hibernateJpaVendorAdapter);
+            entityManagerFactoryBean.setJpaProperties(getHibernateProperties());
+            entityManagerFactoryBean.setPackagesToScan(new String[]{"com.xworkz.tourism.entity"});
+            return entityManagerFactoryBean;
+        }
+
+        @Bean
+        public DataSource dataSource()
+        {
+            DriverManagerDataSource dataSource=new DriverManagerDataSource();
+            dataSource.setDriverClassName(environment.getProperty("jdbc.driverClassName"));
+            dataSource.setUrl(environment.getProperty("jdbc.url"));
+            dataSource.setUsername(environment.getProperty("jdbc.username"));
+            dataSource.setPassword(environment.getProperty("jdbc.password"));
+            return dataSource;
+        }
+
+        public Properties getHibernateProperties()
+        {
+            Properties properties=new Properties();
+            properties.setProperty("hibernate.dialect", environment.getProperty("hibernate.dialect"));
+            properties.setProperty("hibernate.hbm2ddl.auto", environment.getProperty("hibernate.hbm2ddl.auto"));
+            properties.setProperty("hibernate.show_sql", environment.getProperty("hibernate.show_sql"));
+            properties.setProperty("hibernate.format_sql", environment.getProperty("hibernate.format_sql"));
+            return properties;
+        }
+    }
