dcaf265e020078df5478afd5518abb92862f9d9a
Shivani <shivani332433@gmail.com>
2025-11-13 00:57:09 +0530

    saving data

--- tourism-tourism/src/main/java/com/xworkz/tourism/config/TourismConfiguration.java
+++ tourism-tourism/src/main/java/com/xworkz/tourism/config/TourismConfiguration.java
@@ -87,14 +87,6 @@ public InternalResourceViewResolver internalResourceViewResolver() {
     internalResourceViewResolver.setSuffix(".jsp");
     return internalResourceViewResolver;
 }
-@Bean
-public InternalResourceViewResolver internalResourceViewResolver() {
-    System.out.println("InternalResourceViewResolver method");
-    InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
-    internalResourceViewResolver.setPrefix("/");
-    internalResourceViewResolver.setSuffix(".jsp");
-    return internalResourceViewResolver;
-}
 
 
 
